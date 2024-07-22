60d78173bc1c9ff383498a951467c56f7ca82da4
GiraldoSN <geraldonainggolan123@gmail.com>
2024-07-22 19:49:40 +0700

    Ketika diklik nama usernya menampilkan seluruh tulisan yang ditulis oleh user tersebut

--- .history/resources/views/posts.blade_20240722191820.php
+++ .history/resources/views/posts.blade_20240722191820.php
@@ -1,14 +1,13 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-
+    
     @foreach ($posts as $post)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
             <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> |
-                {{ $post->created_at->format('j F Y') }}
+                <a href="/author/{{ $post->author->name }}">{{ $post->author->name }}</a> | {{ $post->created_at->format('j F Y') }} 
             </div>
             <p class="my-4 font-light">
                 {{ Str::limit($post['body'], 50) }}
