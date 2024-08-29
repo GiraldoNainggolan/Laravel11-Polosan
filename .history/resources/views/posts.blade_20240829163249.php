4779b07c11b95100d87454471c9030689f1f7dd8
GiraldoSN <geraldonainggolan123@gmail.com>
2024-08-29 16:35:40 +0700

    Menyesuaikan Blog Section sesuai dengan data category

--- .history/resources/views/posts.blade_20240829163249.php
+++ .history/resources/views/posts.blade_20240829163249.php
@@ -30,7 +30,7 @@
                     <div class="flex justify-between items-center mb-5 text-gray-500">
                         <span
                             class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            {{ $post->category->name }}
+                            Tutorial
                         </span>
                         <span class="text-sm">14 days ago</span>
                     </div>
