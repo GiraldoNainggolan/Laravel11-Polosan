78623cd08c712fa83379040561f3439e1c09b0d9
GiraldoSN <geraldonainggolan123@gmail.com>
2024-08-29 15:18:28 +0700

    Melakukan instalasi Flowbite fitur css

--- .history/tailwind.config_20240713174218.js
+++ .history/tailwind.config_20240713174218.js
@@ -5,7 +5,6 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -14,7 +13,5 @@ export default {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
